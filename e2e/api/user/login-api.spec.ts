4cee3962e8f7eb990fa9622ae8ec7579cdef8396
Lucas Santana <lucassantan017@gmail.com>
2025-12-18 00:06:30 -0300

    adiciona teste tentativa de login sem email e refatora Helper para acessar todas as funções de uma só vez

--- e2e/api/user/login-api.spec.ts
+++ e2e/api/user/login-api.spec.ts
@@ -1,5 +1,5 @@
 import {expect, test} from '@playwright/test'
-import { loginApi } from '../../support/helpers/helpers'
+import * as helpers from '../../support/helpers/helpers'
 import user_data from '../../fixtures/login.json'
 import { LoginUser } from '../../support/models/loginUser.model'
 
@@ -7,7 +7,7 @@ const BASE_URL = process.env.BASE_URL_API
 
 test('deve realizar login', async ({request}) => { 
     const user = user_data.valid as LoginUser
-    const response = await loginApi(request, user)
+    const response = await helpers.loginApi(request, user)
     const body = await response.json()
 
     expect(response.status()).toBe(200)
@@ -17,8 +17,18 @@ test('deve realizar login', async ({request}) => {
 
 test('deve retornar erro ao enviar requisição com credenciais inválidas', async ({request}) => {
     const user = user_data.invalid as LoginUser
-    const response = await loginApi(request, user)
+    const response = await helpers.loginApi(request, user)
     const body = await response.json()
 
     expect(body.error).toBe("Unauthorized")
+    expect(response.status()).toBe(401)
+})
+
+test('deve retornar erro ao enviar requisição com email vazio', async ({request}) => {
+    const user = user_data.invalid_no_email as LoginUser
+    const response = await helpers.loginApi(request, user)
+    const body = await response.json()
+
+    expect(response.status()).toBe(401)
+    expect(body.error).toBe("Invalid login request")
 })
