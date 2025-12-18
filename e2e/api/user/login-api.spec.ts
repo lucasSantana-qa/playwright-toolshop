a46b482dbbff9357778f45bfb2eff9aff68e972b
Lucas Santana <lucassantan017@gmail.com>
2025-12-17 22:21:48 -0300

    refatoração

--- e2e/api/user/login-api.spec.ts
+++ e2e/api/user/login-api.spec.ts
@@ -1,14 +1,22 @@
 import {expect, test} from '@playwright/test'
 import { loginApi } from '../../support/helpers/helpers'
+import user_data from '../../fixtures/login.json'
+import { LoginUser } from '../../support/models/loginUser.model'
 
 const BASE_URL = process.env.BASE_URL_API
 
-test('deve logar como admin', async ({request}) => {    
-    const response = await loginApi(request)
+test('deve realizar login', async ({request}) => { 
+    const user = user_data as LoginUser
+
+    const response = await loginApi(request, user)
 
     const body = await response.json()
 
     expect(response.status()).toBe(200)
     expect(body.access_token).not.toBeNull()
     expect(body.token_type).toBe('bearer')
+})
+
+test('deve retornar erro ao enviar requisição com credenciais inválidas', async ({request}) => {
+
 })
--- e2e/support/helpers/helpers.ts
+++ e2e/support/helpers/helpers.ts
@@ -1,8 +1,9 @@
-import { APIRequestContext, Page } from "@playwright/test";
-import admin from '../../fixtures/login.json'
+import { APIRequestContext } from "@playwright/test";
+// import {UserModel} from '../models/registerUser.model'
+import { LoginUser } from "../models/loginUser.model";
 
 const BASE_URL = process.env.BASE_URL_API
 
-export async function loginApi(request: APIRequestContext) {
-    return await request.post(`${BASE_URL}/users/login`, {data:admin})
+export async function loginApi(request: APIRequestContext, user: LoginUser) {
+    return await request.post(`${BASE_URL}/users/login`, {data: user})
 }
