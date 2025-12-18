a27212d5979b792ceb290347a14a6dbf4d2546de
Lucas Santana <lucassantan017@gmail.com>
2025-12-17 22:53:47 -0300

    adiciona teste de login com credenciais inválidas

--- e2e/api/user/login-api.spec.ts
+++ e2e/api/user/login-api.spec.ts
@@ -6,10 +6,8 @@ import { LoginUser } from '../../support/models/loginUser.model'
 const BASE_URL = process.env.BASE_URL_API
 
 test('deve realizar login', async ({request}) => { 
-    const user = user_data as LoginUser
-
+    const user = user_data.valid as LoginUser
     const response = await loginApi(request, user)
-
     const body = await response.json()
 
     expect(response.status()).toBe(200)
@@ -17,6 +15,10 @@ test('deve realizar login', async ({request}) => {
     expect(body.token_type).toBe('bearer')
 })
 
-test('deve retornar erro ao enviar requisição com credenciais inválidas', async ({request}) => {
+test.only('deve retornar erro ao enviar requisição com credenciais inválidas', async ({request}) => {
+    const user = user_data.invalid as LoginUser
+    const response = await loginApi(request, user)
+    const body = await response.json()
 
+    expect(body.error).toBe("Unauthorized")
 })
